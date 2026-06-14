4d557432b512fca93d44479093b4da50a0d384a4
Maria Smith <maria.t.smith023@gmail.com>
2026-06-13 20:59:52 -0400

    make site responsive

--- src/sections/Projects/ProjectCard.jsx
+++ src/sections/Projects/ProjectCard.jsx
@@ -1,18 +1,20 @@
 export const ProjectCard = ({ name, description, liveLink, sourceLink, img }) => {
   return (
     <div className="project-card">
-      <div className='project-content'>
-        <img src={img} alt='project thumbnail' className='card-thumbnail'/>
-        <h3>{name}</h3>
-        <div className='project-description'>{description}</div>
-      </div>
-      <div className='project-links'>
-        {liveLink &&
-          <a href={liveLink} target='_blank'>LIVE DEMO</a>
-        }
-        {sourceLink && (
-          <a href={sourceLink} target='_blank'>VIEW SOURCE</a>
-        )}
+      <img src={img} alt='project thumbnail' className='card-thumbnail'/>
+      <div className='project-body'>
+        <div className='project-content'>
+          <h3>{name}</h3>
+          <div className='project-description'>{description}</div>
+        </div>
+        <div className='project-links'>
+          {liveLink &&
+            <a href={liveLink} target='_blank'>LIVE DEMO</a>
+          }
+          {sourceLink && (
+            <a href={sourceLink} target='_blank'>VIEW SOURCE</a>
+          )}
+        </div>
       </div>
     </div>
   )
